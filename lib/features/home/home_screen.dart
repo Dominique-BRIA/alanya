6d8eaa016a8adc1923db59ab4eda19cded538287
Dominique-BRIA <236426024+Dominique-BRIA@users.noreply.github.com>
2026-07-27 00:46:18 +0000

    Fix chat : restore original list + import alanya_id_formatter
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../../l10n/app_localizations.dart';
+import '../../core/alanya_id_formatter.dart';
 
 import '../../core/connectivity_service.dart';
 import '../../core/conversation_cache.dart';
